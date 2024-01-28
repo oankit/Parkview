b850ac3182dbcfcd973f6aab14d407bf5f682a93
Omar Ankit <omar.ankit@hotmail.com>
2024-01-28 03:11:10 -0800

    Connected the map to the database, enabling live map updates

--- ParkViewApp/src/ParkingLotMap/ParkingLotMap.tsx
+++ ParkViewApp/src/ParkingLotMap/ParkingLotMap.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Image, Dimensions, ScrollView } from 'react-native';
+import database from '@react-native-firebase/database'; // Import the Firebase database module
+import carImage from './car.png';
 import parkingSpacesData from './ParkingSpaces.json';
-import carImage from './car.png'; 
 
 const { width: screenWidth } = Dimensions.get('window');
 
@@ -9,19 +10,29 @@ const ParkingLotMap = () => {
   const [occupiedSpaces, setOccupiedSpaces] = useState(new Map());
 
   useEffect(() => {
-    const newOccupiedSpaces = new Map();
-    parkingSpacesData.forEach((_, index) => {
-      newOccupiedSpaces.set(index, Math.random() < 0.5); 
-    });
-    setOccupiedSpaces(newOccupiedSpaces);
-  }, []);
+    const reference = database().ref('/occupied_spaces');
 
+    // Listen for changes in the /occupied_spaces path
+    const onDataChange = reference.on('value', snapshot => {
+      const dataFromDatabase = snapshot.val();
+      const newOccupiedSpaces = new Map();
+
+      parkingSpacesData.forEach((_, index) => {
+        newOccupiedSpaces.set(index, dataFromDatabase && dataFromDatabase[index]);
+      });
+
+      setOccupiedSpaces(newOccupiedSpaces);
+    });
+
+    // Unsubscribe from the listener when the component is unmounted
+    return () => reference.off('value', onDataChange);
+  }, []);
 
   const maxWidth = Math.max(...parkingSpacesData.map(s => s.position[0] + s.width));
   const maxHeight = Math.max(...parkingSpacesData.map(s => s.position[1] + s.height));
   const scaleX = screenWidth / maxWidth;
-  const scaleY = scaleX; 
-  const spaceGap = 0; // Gap between spaces for parking lines
+  const scaleY = scaleX;
+  const spaceGap = 0;
 
   return (
     <ScrollView style={styles.scrollView} contentContainerStyle={styles.contentContainer}>
@@ -56,9 +67,7 @@ const ParkingLotMap = () => {
   );
 };
 
-
 const renderParkingLines = (parkingSpacesData, scaleX, scaleY, spaceGap) => {
-  
   return parkingSpacesData.map((space, index) => (
     <View
       key={`line-${index}`}
@@ -68,7 +77,7 @@ const renderParkingLines = (parkingSpacesData, scaleX, scaleY, spaceGap) => {
         top: space.position[1] * scaleY,
         width: spaceGap,
         height: space.height * scaleY,
-        backgroundColor: 'yellow', 
+        backgroundColor: 'yellow',
       }}
     />
   ));
@@ -88,7 +97,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderWidth: 1,
     borderColor: '#000',
-    borderRadius: 4, 
+    borderRadius: 4,
   },
   car: {
     width: '100%',
